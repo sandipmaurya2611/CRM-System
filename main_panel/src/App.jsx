a6040bc85e2623a088a4bf2efc96cf00419f869c
Vishalmagars <Visshalmagar9579@gmail.com>
2025-11-13 14:46:58 +0530

    add table

--- main_panel/src/App.jsx
+++ main_panel/src/App.jsx
@@ -5,12 +5,15 @@ import SuperAdminDashboard from './pages/Dashboard/mainDashboard';
 import MainSubscription from './pages/Subscription/mainSubscription';
 import Payment from './pages/Payment/Payment';
 
-
 function App() {
   return (
     <div className="flex h-screen bg-gray-100">
       <Sidebar />
-      <main className="flex-1 overflow-auto">
+
+      <main
+        className="flex-1 overflow-auto"
+        style={{ marginLeft: "240px" }}   // ← ADD THIS
+      >
         <Routes>
           <Route path="/dashboard" element={<SuperAdminDashboard />} />
           <Route path="/" element={<SuperAdminDashboard />} />
